04af3b31d08587e99f15976071c116019d09fb5d
Josh Wright <joshwrightaudio@gmail.com>
2021-05-15 13:11:44 -0400

    api works with body

--- react-user-directory/src/components/Table/Table.js
+++ react-user-directory/src/components/Table/Table.js
@@ -1,21 +1,23 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
+
 
 function Table(props) {
   console.log(props);
   // The data was coming in with the error because I didn't wrap it in JSX.
   const userData = props.users.map((user) => {
-    return <div>{user.name.first}</div>;
+    return (
+     
+      <div>
+          <Avatar src={user.picture.thumbnail}></Avatar>
+        {"NAME: " + user.name.first}
+        {" " + user.name.last + " "}
+        {"GENDER:" + " " + user.gender}
+      </div>
+    );
   });
   console.log("This is the userData after returning as a div", userData);
-  return (
-    <div className="container">
-      {userData}
-      {/* You can run the function above inside the return as a jsx expression or do a function call above  */}
-      {/* {props.users.map((user) => {
-				return <div>{user.name.first}</div>;
-			})} */}
-    </div>
-  );
+  return <div className="container">{userData}</div>;
 }
 
 export default Table;
